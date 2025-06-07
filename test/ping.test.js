afa54a6192d63ec209ba308072f02a8c1e6ccdda
Zeke142 <dirthaul142@gmail.com>
2025-06-06 19:48:06 -0700

    Update ping.test.js

--- test/ping.test.js
+++ test/ping.test.js
@@ -1,1 +1,13 @@
+import { describe, it, expect } from 'vitest';
+
+describe('Ping Test', () => {
+  it('should respond with pong', () => {
+    const res = { json: (data) => data };
+    const expected = { message: 'pong' };
+    const pingHandler = (req, res) => res.json(expected);
+
+    const result = pingHandler(null, res);
+    expect(result).toEqual(expected);
+  });
+});
 
